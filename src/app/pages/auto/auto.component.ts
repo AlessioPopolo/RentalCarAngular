8569b05ec2855b02bab3068fe8339df0bcc97ad3
AlessioPopolo <alessio.popolo@si2001.it>
2021-08-19 11:14:43 +0200

    REFACTOR remove unused imports

--- src/app/pages/auto/auto.component.ts
+++ src/app/pages/auto/auto.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {autoTableConfig} from "../../resources/AutoTableConfig";
-import {Auto, Utente} from "../../service/in-memory-data.service";
-import {UtenteService} from "../../service/utente.service";
+import {Auto} from "../../service/in-memory-data.service";
 
 @Component({
   selector: 'app-auto',
